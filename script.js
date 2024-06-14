f2e6bc4e91d8aabe7b594608f35178c83a501ae9
manishgyawali <manishgyawali345@gmail.com>
2024-06-14 17:08:18 +0545

    new commit

--- script.js
+++ script.js
@@ -2,23 +2,46 @@
 
 let username="";
 let email="";
-let pasword="";
+let password="";
 
-Username = (a)=>{
+const Username = (a)=>{
 username=a;
 }
 
-Email = (a)=>{
+const Email = (a)=>{
 email=a;
 }
-Password =(a)=>{
-pasword=a;
+const Password =(a)=>{
+    password=a;
 }
-submit =()=>{
-console.log(username);
-console.log(email);
+let usernameError=document.getElementById('usernameError');
+let emailError=document.getElementById('emailError');
+let passwordError=document.getElementById('passwordError');
 
-console.log(pasword);
+submit =()=>{
+
+if(username.length>0 && email.length>0 && password.length>0){
+    if(username.length<=10){
+        usernameError.innerText='Username must be 10 characters !';
+    }
+    if(email.length<=10){
+        emailError.innerText='Email must be 10 characters !';
+    }
+    if(password.length<=10){
+        passwordError.innerText='Password must be 10 characters !';
+    }
+}
+else{
+    if(username==0){
+        usernameError.innerText='Username cannot be empty !';
+    }
+    if(email==0){
+        emailError.innerText='Email cannot be empty !';
+    }
+    if(password==0){
+        passwordError.innerText='Password cannot be empty !';
+    }
+}
 }
 
 
